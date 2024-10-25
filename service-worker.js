1366ec3dd6f3ad3b37bed9ed1b8a1030e5c6d273
MarylinAccesorios <marilyn.dev.accesory@gmail.com>
2024-10-24 21:22:20 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -32,11 +32,6 @@ self.addEventListener('push', function(event) {
             } else {
                 notificationMessage = 'Cerraste la App, Tenes una notificacion!!!';
             }
-
-            // Show a notification with title 'ServiceWorker Cookbook' and body depending
-            // on the state of the clients of the service worker (three different bodies:
-            // 1, the page is focused; 2, the page is still open but unfocused; 3, the page
-            // is closed).
             return self.registration.showNotification(title, {
                 body: notificationMessage,
             });
@@ -53,6 +48,7 @@ self.addEventListener('notificationclick', function(event) {
             if (clientList.length > 0) {
                 return clientList[0].focus();
             }
+
 
             // Otherwise, open a new page.
             return self.clients.openWindow('../push-clients_demo.html');
